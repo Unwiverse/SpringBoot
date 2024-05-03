789a742bf2647c25ea861550d30952b11ca2f2ae
goott3 <eomseunghyeon480@gmail.com>
2024-05-03 18:16:26 +0900

    gg

---
+++ EComponents/src/main/java/com/goott/EComponents/EComponentsApplication.java
@@ -0,0 +1,13 @@
+package com.goott.EComponents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EComponentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EComponentsApplication.class, args);
+	}
+
+}
---
+++ EComponents/src/test/java/com/goott/EComponents/EComponentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goott.EComponents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EComponentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
